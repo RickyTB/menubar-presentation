b2627752a13eaf9a97d6f1a98d63c2fd2a41a99c
Ricardo Baquero <wola_ricky@hotmail.com>
2020-01-09 17:18:59 -0500

    - Styles descriptions translated.

--- src/pages/4Ajax.js
+++ src/pages/4Ajax.js
@@ -27,7 +27,8 @@ const Ajax = () => {
                 <div className="col-md-6">
                     <Title align="left" color="white" className="pt-4 pb-2">Estilos</Title>
                     <Card>
-                        <p>Se puede agregar estilos en línea en todos los componentes, y preferiblemente se puede referir a las clases de css de la siguiente lista:</p>
+                        <p>Se puede agregar estilos en línea en todos los componentes, y preferiblemente se puede
+                            referir a las clases de css de la siguiente lista:</p>
                         <table className="table">
                             <thead>
                             <tr>
@@ -36,11 +37,26 @@ const Ajax = () => {
                             </tr>
                             </thead>
                             <tbody>
-                            <tr> <td>ui-menubar</td> <td>Container element of menubar.</td></tr>
-                            <tr> <td>ui-menu-list</td> <td>List container</td></tr>
-                            <tr> <td>ui-menuitem</td> <td>Each menu item</td></tr>
-                            <tr> <td>ui-menuitem-link</td> <td>Anchor element in a link item</td></tr>
-                            <tr> <td>ui-menuitem-text</td> <td>Text element in an item</td></tr>
+                            <tr>
+                                <td>.ui-menubar</td>
+                                <td>Elemento contenedor del menubar.</td>
+                            </tr>
+                            <tr>
+                                <td>.ui-menu-list</td>
+                                <td>Contenedor de lista.</td>
+                            </tr>
+                            <tr>
+                                <td>.ui-menuitem</td>
+                                <td>Cada menuitem.</td>
+                            </tr>
+                            <tr>
+                                <td>.ui-menuitem-link</td>
+                                <td>El elemento "a" en un item link.</td>
+                            </tr>
+                            <tr>
+                                <td>.ui-menuitem-text</td>
+                                <td>Elemento de texto en un item.</td>
+                            </tr>
                             </tbody>
                         </table>
                     </Card>
